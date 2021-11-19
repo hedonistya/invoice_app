8e4c320cd0b834b14743c772700d0782e96d25ad
nikitatopolskov <nikitatopolskov150@gmail.com>
2021-11-20 01:19:06 +0300

    first commit

---
+++ src/components/Invoice.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Invoice"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/InvoiceModal.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "InvoiceModal"
+}
+</script>
+
+<style scoped>
+
+</style>
